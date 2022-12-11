66f1fe96b454da6c37fac14c0eb86c893a45b1b3
Waldemar Tomme <waldetomme@gmail.com>
2022-12-11 14:43:09 +0100

    Create new build file

--- build.ps1
+++ build.ps1
@@ -2,7 +2,6 @@ $ErrorActionPreference = 'Stop'
 
 Set-Location -LiteralPath $PSScriptRoot
 
-$env:CAKE_SETTINGS_SKIPPACKAGEVERSIONCHECK = 'true' # only temporarily
 $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = '1'
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
 $env:DOTNET_NOLOGO = '1'
